a9cd8922b6e5247550394de9eae27d920f6707e7
fs-atGithub <filip.sostaric@protonmail.com~>
2024-10-25 23:38:08 +0200

    ESLint & Prettier setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl text-center justify-center">
+    <h1 className="justify-center text-center text-3xl font-black text-white">
       Welcome to the world of NextJS 15
     </h1>
   );
