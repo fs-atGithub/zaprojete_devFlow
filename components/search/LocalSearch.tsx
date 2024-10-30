caad9a46822be37b60b9192de7cef8d60b4e885b
fs-atGithub <filip.sostaric@protonmail.com~>
2024-10-30 13:49:10 +0100

    Implemented local Searchzbar and Card

---
+++ components/search/LocalSearch.tsx
@@ -0,0 +1,66 @@
+"use client";
+import { clearTimeout } from "timers";
+
+import Image from "next/image";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+import { formUrlQuery, removeKeysFromUrlQuery } from "@/lib/url";
+
+import { Input } from "../ui/input";
+
+interface Props {
+  route: string;
+  imgSrc: string;
+  placeholder: string;
+  otherClasses?: string;
+}
+
+const LocalSearch = ({ route, imgSrc, placeholder, otherClasses }: Props) => {
+  const searchParams = useSearchParams();
+  const query = searchParams.get("query") || "";
+
+  const [searchQuery, setSearchQuery] = useState(query);
+
+  const router = useRouter();
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const delayDebaunceFn = setTimeout(() => {
+      if (searchQuery) {
+        const newURL = formUrlQuery({
+          params: searchParams.toString(),
+          key: "query",
+          value: searchQuery,
+        });
+        router.push(newURL, { scroll: false });
+      } else {
+        if (pathname === route) {
+          const newURL = removeKeysFromUrlQuery({
+            params: searchParams.toString(),
+            keysToRemove: ["query"],
+          });
+          router.push(newURL, { scroll: false });
+        }
+        return () => clearTimeout(delayDebaunceFn);
+      }
+    }, 300);
+  }, [searchQuery, searchParams, router, route, pathname]);
+
+  return (
+    <div
+      className={`background-light800_darkgradient flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4 ${otherClasses}`}
+    >
+      <Image src={imgSrc} width={24} height={24} alt="Search" />
+      <Input
+        className="paragraph-regular no-focus placeholder text-dark400_light700 border-none shadow-none outline-none"
+        type="text"
+        placeholder={placeholder}
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+    </div>
+  );
+};
+
+export default LocalSearch;
